e2409df72cb9bb84247e966e895b39a3dbd25469
정경수 <soo@jeong-gyeongsuui-MacBookPro.local>
2022-09-06 18:31:29 +0900

    스프링 데이터 JPA 끝

--- SpringDbAdvance/itemservice-db-start/src/main/java/hello/itemservice/domain/Item.java
+++ SpringDbAdvance/itemservice-db-start/src/main/java/hello/itemservice/domain/Item.java
@@ -2,12 +2,19 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "item_index")
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+
+
     private Integer price;
     private Integer quantity;
 
