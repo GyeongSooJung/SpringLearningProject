cc5c89ace7679e856d0d0e610db80ac9d2b06767
정경수 <soo@jeong-gyeongsuui-MacBookPro.local>
2022-09-13 18:24:56 +0900

    프록시 개념 끝

--- SpringAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,13 +1,23 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class,AppV2Config.class})
+//@Import({AppV1Config.class,AppV2Config.class})
+//@Import(DynamicProxyBasicConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
@@ -15,4 +25,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
